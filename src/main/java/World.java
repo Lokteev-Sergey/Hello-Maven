f267da534735973b195d2f995a065e04cd7fcc18
Сергей Локтеев <listvol@mail.ru>
2020-02-19 22:16:03 +0500

    Find dog

--- src/main/java/World.java
+++ src/main/java/World.java
@@ -1,6 +1,9 @@
 
 public class World {
     public static void main(String[] args) {
-        System.out.print("Hello world");
+        System.out.println("Hello world");
+        Dog newton = new Dog();
+        newton.setName("Newton");
+        System.out.println(newton.getName());
     }
 }
